2f60d1d8237ef67fa8a6d801331dea8b7ec7c573
lovesuper <antononrails@gmail.com>
2019-03-14 02:21:18 +0300

    New rules for obtaining quotes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from json import dumps
 
-from bottle import run, response, Bottle
+from bottle import run, response, Bottle, request
 from sqlalchemy import func
 
 from db import DBSession, Quote
@@ -10,14 +10,25 @@ app = Bottle()
 
 @app.route("/")
 def root():
-    return "Welcome!"
+    return ("Use `/quotes` endpoint with optional `limit` and `offset` query parameters "
+            "to get some random quotes from `bash.im`, `zadolba.li` or `ithappens.me`.")
 
 
 @app.route("/quotes")
 def get_quotes():
     response.content_type = "application/json"
+    limit = request.query.limit or 100
+    offset = request.query.offset or 0
+
+    try:
+        limit = int(limit)
+        offset = int(offset)
+        assert 0 <= int(limit) <= 1000 or 0 < int(offset)
+    except Exception:
+        return dumps({"Error": "Too much you wish."})
+
     session = DBSession()
-    quotes = session.query(Quote).order_by(func.random()).all()
+    quotes = session.query(Quote).order_by(func.random()).limit(limit).offset(offset).all()
 
     return dumps(list(map(lambda x: x.jsonify(), quotes)))
 
