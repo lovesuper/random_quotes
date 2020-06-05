d02c6da28fb17c3f4b0ee7cbf0eb56d34e6a4356
lovesuper <antononrails@gmail.com>
2020-06-05 11:30:49 +0300

    minor fixes

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 rm -rf .env
-virtualenv -p python3 .env --no-site-packages
+virtualenv -p python3 .env
 . .env/bin/activate
 pip install -r requirements.txt
 export DATABASE_URL=postgresql://user:user@localhost/quotes_db
